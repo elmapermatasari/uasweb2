4f7e031033f0868a031450ae829e376d59a6f4e8
elmapermatasari <159127591+elmapermatasari@users.noreply.github.com>
2024-02-06 17:10:40 +0700

    1
    1

---
+++ application/views/home.php
@@ -0,0 +1,13 @@
+<?php $this->load->view('header'); ?> 
+
+<div class="row">
+    <div class="container"> 
+        <!-- Main component for a primary marketing message or call to action --> 
+        <div class="jumbotron"> 
+            <h1>INI ADALAH POS TOKO ELMA</h1>
+            <p>SYSTEM PENJUALAN TOKO ELMA</p>
+        </div>
+    </div> 
+</div>
+<!-- /container --> 
+<?php $this->load->view('footer'); ?> 
